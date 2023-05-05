8991fa8e4a975c37de073744a8569953b82bfffc
Daimonix <dimaprotsap@gmail.com>
2023-05-05 13:28:48 +0300

    Added url check

--- miniprogram/pages/display/page2Canvas/page2Canvas.ts
+++ miniprogram/pages/display/page2Canvas/page2Canvas.ts
@@ -1,7 +1,7 @@
 const fields = {
   rect: true,
   size: true,
-  computedStyle: ['color', 'backgroundColor', 'background', 'font'],
+  computedStyle: ['color', 'backgroundColor', 'background', 'font', 'wordWrap'],
   properties: ['src'],
   dataset: true
 } as WechatMiniprogram.Fields;
@@ -14,8 +14,6 @@ type Offset = {
 }
 
 function drawText(ctx: any, text: string, x: number, y: number, lineHeight: number, targetWidth: number) {
-  targetWidth = targetWidth || 0;
-
   if (targetWidth <= 0) {
     ctx.fillText(text, x, y);
     return;
@@ -40,6 +38,23 @@ function drawText(ctx: any, text: string, x: number, y: number, lineHeight: numb
     ctx.fillText(words.join(' '), x, y + (lineHeight * currentLine));
 }
 
+function fitText(ctx: any, text: string, x: number, y: number, lineHeight: number, fitWidth: number) {
+  if (fitWidth <= 0) {
+    ctx.fillText(text, x, y);
+    return;
+  }
+
+  for (let i = 1; i <= text.length; i++) {
+    const str = text.substr(0, i);
+    if (ctx.measureText(str).width > fitWidth) {
+      ctx.fillText(text.substr(0, i - 1), x, y);
+      fitText(ctx, text.substr(i - 1), x, y + lineHeight, lineHeight, fitWidth);
+      return;
+    }
+  }
+  ctx.fillText(text, x, y);
+}
+
 function drawImage(canvas: WechatMiniprogram.Canvas, ctx: any, src: string, x: number, y: number, width: number, height: number) {
   const img = canvas.createImage();
   img.src = src;
@@ -57,48 +72,18 @@ function drawElement(ctx: any, element: Record<string, any>, offset?: Offset, ro
   ctx.fillRect(root ? 0 : element.left - offset.left, root ? 0 : element.top - offset.top, element.width, element.height);
   if (element.dataset?.text) {
     ctx.fillStyle = element.color;
-    ctx.font = `100 ${parseInt(element.font)}px Arial`;
-    drawText(ctx, element.dataset.text, element.left - offset.left, element.top - offset.top,
-      parseInt(element.font), element.width);
+    ctx.font = `200 ${parseInt(element.font)}px Arial`;
+    if (element.wordWrap === "break-word")
+      fitText(ctx, element.dataset.text, element.left - offset.left, element.top - offset.top,
+        parseInt(element.font), element.width);
+    else
+      drawText(ctx, element.dataset.text, element.left - offset.left, element.top - offset.top,
+        parseInt(element.font), element.width);
   }
 }
 
-function createGradient(ctx: any, element: Record<string, any>, offset: Offset) {
-  const gradient = ctx.createLinearGradient(element.left - offset.left, element.top - offset.top,
-    element.right - offset.right, element.bottom - offset.bottom);
-  const str = element.background as string;
-  const gIndex = str.indexOf("gradient");
-  let s = str.indexOf(",", gIndex) + 2;
-  let e = str.indexOf("%)", gIndex) + 1;
-  const stops = str.substring(s, e).split('%, ');
-  stops.forEach((stop) => {
-    const ioS = stop.lastIndexOf(' ');
-    const color = stop.substring(0, ioS);
-    const val = parseFloat(stop.substring(ioS).replace("%", "")) / 100;
-    gradient.addColorStop(val, color);
-  });
-  ctx.fillStyle = gradient;
-}
-
 function draw(canvas: WechatMiniprogram.Canvas, scale: number, containerProps: Record<string, any>, childProps: Record<string, any>, onReady: () => void) {
-  const offset = { left: containerProps.left, top: containerProps.top, right: containerProps.right, bottom: containerProps.bottom } as Offset;
-  const pendingImages = Array<Promise<any>>();
-  const width = containerProps.width * scale;
-  const height = containerProps.height * scale;
-  canvas.width = width;
-  canvas.height = height;
-  const ctx = canvas.getContext('2d');
-  ctx.scale(scale, scale);
-  drawElement(ctx, containerProps, undefined, true);
-
-  childProps.forEach((child: Record<string, any>) => {
-    drawElement(ctx, child, offset);
-    if (child.src)
-      pendingImages.push(
-        drawImage(canvas, ctx, child.src, child.left - offset.left, child.top - offset.top, child.width, child.height)
-      );
-  });
-  Promise.all(pendingImages).then(onReady);
+  drawAsync(canvas, scale, containerProps, childProps).then(onReady);
 }
 
 export function renderPageOnCanvas(canvas: WechatMiniprogram.Canvas, containerSelector: string, elementsToRenderSelector: string,
@@ -133,13 +118,13 @@ function drawAsync(canvas: WechatMiniprogram.Canvas, scale: number, containerPro
         drawImage(canvas, ctx, child.src, child.left - offset.left, child.top - offset.top, child.width, child.height)
       );
   });
-  
+
   return new Promise((resolve) => {
     Promise.all(pendingImages).then(resolve);
   });
 }
 
-export function renderPageOnCanvasAsync(canvas: WechatMiniprogram.Canvas, containerSelector: string, elementsToRenderSelector: string, scale = 4) {
+export async function renderPageOnCanvasAsync(canvas: WechatMiniprogram.Canvas, containerSelector: string, elementsToRenderSelector: string, scale = 4) {
   const query = wx.createSelectorQuery();
 
   const container = new Promise<Record<string, any>>((resolve) => {
@@ -149,5 +134,6 @@ export function renderPageOnCanvasAsync(canvas: WechatMiniprogram.Canvas, contai
     query.selectAll(elementsToRenderSelector).fields(fields, (res) => resolve(res)).exec()
   });
 
-  return Promise.all([container, elements]).then((res) => drawAsync(canvas, scale, res[0], res[1]));
+  const res_2 = await Promise.all([container, elements]);
+  return await drawAsync(canvas, scale, res_2[0], res_2[1]);
 }
--- miniprogram/pages/index/index.ts
+++ miniprogram/pages/index/index.ts
@@ -3,6 +3,10 @@ type Input = {
   name: string
 }
 
+function isURL(str : string) {
+  return /^(?:\w+:)?\/\/([^\s\.]+\.\S{2}|localhost[\:?\d]*)\S*$/.test(str); 
+}
+
 const inputFields = [{ id: 0, name: "Name" },
 { id: 1, name: "Article title" },
 { id: 2, name: "Article URL" }] as Array<Input>;
@@ -11,18 +15,21 @@ Page({
   data: {
     inputs: inputFields,
     values: new Array<string>(inputFields.length).fill(""),
-    allValueSet: false
+    allSet: false,
+    validUrl: false
   },
   inputChange(e: WechatMiniprogram.CustomEvent) {
     const values = this.data.values;
     values[e.currentTarget.dataset.id] = e.detail.value;
+    this.validate();
+  },
+  validate() {
     let allSet = true;
-    values.forEach(v => allSet = allSet && v.length > 0);
+    this.data.values.forEach(v => allSet = allSet && v.length > 0);
     this.setData({
-      values: values,
-      allValueSet: allSet
+      allSet: allSet,
+      validUrl: isURL(this.data.values[this.data.values.length - 1])
     });
-    
   },
   create() {
     const app = getApp<IAppOption>();
